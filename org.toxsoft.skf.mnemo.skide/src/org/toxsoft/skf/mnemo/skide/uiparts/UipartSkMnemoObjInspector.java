d476bac52a18c84287fcd961747da451d765b037
vs <vs@LAPTOP-DSEKI2TA>
2023-06-30 11:30:29 +0300

    Убрана зависимость от песочницы

--- org.toxsoft.skf.mnemo.skide/src/org/toxsoft/skf/mnemo/skide/uiparts/UipartSkMnemoObjInspector.java
+++ org.toxsoft.skf.mnemo.skide/src/org/toxsoft/skf/mnemo/skide/uiparts/UipartSkMnemoObjInspector.java
@@ -3,20 +3,16 @@ package org.toxsoft.skf.mnemo.skide.uiparts;
 import org.eclipse.swt.widgets.*;
 import org.toxsoft.core.tsgui.mws.bases.*;
 import org.toxsoft.core.tsgui.utils.layout.*;
-import org.toxsoft.skf.mnemo.gui.inspector.*;
 
 public class UipartSkMnemoObjInspector
     extends MwsAbstractPart {
 
   // VedInspectorPanel inspectorPanel;
-  InspectorPanel inspectorPanel;
 
   @Override
   protected void doInit( Composite aParent ) {
 
     aParent.setLayout( new BorderLayout() );
-    inspectorPanel = new InspectorPanel( aParent, tsContext() );
-    inspectorPanel.setLayoutData( BorderLayout.CENTER );
   }
 
   // ------------------------------------------------------------------------------------
