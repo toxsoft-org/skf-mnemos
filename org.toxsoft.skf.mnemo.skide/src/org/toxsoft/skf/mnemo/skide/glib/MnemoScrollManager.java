4859616349ce4549c84feab3ff252bab92f48a81
vs <vs@LAPTOP-DSEKI2TA>
2023-12-10 18:12:36 +0300

    Временно убрана зависимость то sandbox

--- org.toxsoft.skf.mnemo.skide/src/org/toxsoft/skf/mnemo/skide/glib/MnemoScrollManager.java
+++ org.toxsoft.skf.mnemo.skide/src/org/toxsoft/skf/mnemo/skide/glib/MnemoScrollManager.java
@@ -2,12 +2,8 @@ package org.toxsoft.skf.mnemo.skide.glib;
 
 import org.eclipse.swt.events.*;
 import org.eclipse.swt.widgets.*;
-import org.toxsoft.core.tsgui.graphics.*;
 import org.toxsoft.core.tsgui.ved.screen.*;
 import org.toxsoft.core.tslib.bricks.d2.*;
-import org.toxsoft.core.tslib.bricks.geometry.*;
-import org.toxsoft.core.tslib.bricks.geometry.impl.*;
-import org.toxsoft.sandbox.test.goga.vp.*;
 
 /**
  * Вспомогательный класс для реализации скроллинга мнемосхем.
@@ -16,13 +12,13 @@ import org.toxsoft.sandbox.test.goga.vp.*;
  */
 public class MnemoScrollManager {
 
-  private final IViewportCalculator vpCalc = new ViewportCalculator( new CalculationStrategySettings( //
-      ETsFulcrum.LEFT_TOP, //
-      EVpFulcrumUsageStartegy.INSIDE, //
-      EVpBoundingStrategy.CONTENT, //
-      new TsPoint( 10, 10 ), //
-      false //
-  ) );
+  // private final IViewportCalculator vpCalc = new ViewportCalculator( new CalculationStrategySettings( //
+  // ETsFulcrum.LEFT_TOP, //
+  // EVpFulcrumUsageStartegy.INSIDE, //
+  // EVpBoundingStrategy.CONTENT, //
+  // new TsPoint( 10, 10 ), //
+  // false //
+  // ) );
 
   private final IVedScreen vedScreen;
 
@@ -40,8 +36,8 @@ public class MnemoScrollManager {
 
     vedView.configChangeEventer().addListener( aSource -> {
       ID2Point size = vedView.canvasConfig().size();
-      vpCalc.setContentSize( new D2Size( size.x(), size.y() ) );
-      vpCalc.queryConversionChange( vedView.getConversion() );
+      // vpCalc.setContentSize( new D2Size( size.x(), size.y() ) );
+      // vpCalc.queryConversionChange( vedView.getConversion() );
     } );
 
     canvas = (Canvas)vedView.getControl();
@@ -51,10 +47,10 @@ public class MnemoScrollManager {
 
       @Override
       public void widgetSelected( SelectionEvent aE ) {
-        ScrollBarSettings sbs1 = vpCalc.output().horBarSettings();
-        ScrollBarSettings sbs2 = ScrollBarSettings.of( hBar );
-        int queryDelta = sbs2.selection() - sbs1.selection();
-        vpCalc.queryToShift( queryDelta, 0 );
+        // ScrollBarSettings sbs1 = vpCalc.output().horBarSettings();
+        // ScrollBarSettings sbs2 = ScrollBarSettings.of( hBar );
+        // int queryDelta = sbs2.selection() - sbs1.selection();
+        // vpCalc.queryToShift( queryDelta, 0 );
       }
 
     } );
@@ -64,10 +60,10 @@ public class MnemoScrollManager {
 
       @Override
       public void widgetSelected( SelectionEvent aE ) {
-        ScrollBarSettings sbs1 = vpCalc.output().verBarSettings();
-        ScrollBarSettings sbs2 = ScrollBarSettings.of( vBar );
-        int queryDelta = sbs2.selection() - sbs1.selection();
-        vpCalc.queryToShift( 0, queryDelta );
+        // ScrollBarSettings sbs1 = vpCalc.output().verBarSettings();
+        // ScrollBarSettings sbs2 = ScrollBarSettings.of( vBar );
+        // int queryDelta = sbs2.selection() - sbs1.selection();
+        // vpCalc.queryToShift( 0, queryDelta );
       }
 
     } );
@@ -76,30 +72,30 @@ public class MnemoScrollManager {
 
       @Override
       public void controlResized( ControlEvent aE ) {
-        ITsRectangle vpBounds = TsGraphicsUtils.tsFromRect( canvas.getClientArea() );
-        if( vpCalc.setViewportBounds( vpBounds ) ) {
-          vedScreen.view().redraw();
-        }
+        // ITsRectangle vpBounds = TsGraphicsUtils.tsFromRect( canvas.getClientArea() );
+        // if( vpCalc.setViewportBounds( vpBounds ) ) {
+        // vedScreen.view().redraw();
+        // }
       }
 
       @Override
       public void controlMoved( ControlEvent aE ) {
-        ITsRectangle vpBounds = TsGraphicsUtils.tsFromRect( canvas.getClientArea() );
-        if( vpCalc.setViewportBounds( vpBounds ) ) {
-          vedScreen.view().redraw();
-        }
+        // ITsRectangle vpBounds = TsGraphicsUtils.tsFromRect( canvas.getClientArea() );
+        // if( vpCalc.setViewportBounds( vpBounds ) ) {
+        // vedScreen.view().redraw();
+        // }
       }
     } );
 
-    vpCalc.output().genericChangeEventer().addListener( s -> whenCalculatorOutputChanges() );
+    // vpCalc.output().genericChangeEventer().addListener( s -> whenCalculatorOutputChanges() );
 
   }
 
   private void whenCalculatorOutputChanges() {
-    vpCalc.output().horBarSettings().applyTo( hBar );
-    vpCalc.output().verBarSettings().applyTo( vBar );
-    vedView.configChangeEventer().pauseFiring();
-    vedView.setConversion( vpCalc.output().conversion() );
+    // vpCalc.output().horBarSettings().applyTo( hBar );
+    // vpCalc.output().verBarSettings().applyTo( vBar );
+    // vedView.configChangeEventer().pauseFiring();
+    // vedView.setConversion( vpCalc.output().conversion() );
     vedView.redraw();
     vedView.configChangeEventer().resumeFiring( false );
   }
