06b832af42187517c3e9bb75315e0ff4f9651629
goga <goga@GOGAL-LIN>
2024-01-31 13:11:48 +0300

    SkIDE unit categories tree added to main window

--- org.toxsoft.skf.mnemo.skide/src/org/toxsoft/skf/mnemo/skide/main/SkideUnitMnemo.java
+++ org.toxsoft.skf.mnemo.skide/src/org/toxsoft/skf/mnemo/skide/main/SkideUnitMnemo.java
@@ -5,6 +5,7 @@ import static org.toxsoft.core.tslib.av.metainfo.IAvMetaConstants.*;
 import static org.toxsoft.skf.mnemo.skide.ISkidePluginMnemoConstants.*;
 import static org.toxsoft.skf.mnemo.skide.ISkidePluginMnemoSharedResources.*;
 import static org.toxsoft.skide.core.ISkideCoreConstants.*;
+import static org.toxsoft.skide.core.api.ucateg.ISkideUnitCategoryConstants.*;
 
 import org.toxsoft.core.tsgui.bricks.ctx.*;
 import org.toxsoft.core.tslib.av.opset.impl.*;
@@ -32,6 +33,7 @@ public class SkideUnitMnemo
     super( UNIT_ID, OptionSetUtils.createOpSet( //
         TSID_NAME, STR_SKIDE_MNEMOS_UNIT, //
         TSID_DESCRIPTION, STR_SKIDE_MNEMOS_UNIT_D, //
+        OPDEF_SKIDE_UNIT_CATEGORY, UCATEGID_SYSDESCR, //
         TSID_ICON_ID, ICONID_SKIDE_UNIT_MNEMOS_EDITOR //
     ), aContext, aCreator );
     unitActions().add( ACDEF_ABOUT );
