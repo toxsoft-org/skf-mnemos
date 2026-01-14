73b901e01163fa5886fce334cf709bc256a67c94
goga <goga@GOGA-MPC-LIN>
2026-01-14 03:10:20 +0300

    Working on PRO and LITE Mnemo editors, part 1

--- org.toxsoft.skf.mnemo.skide/src/org/toxsoft/skf/mnemo/skide/main/SkideUnitMnemoPanel.java
+++ org.toxsoft.skf.mnemo.skide/src/org/toxsoft/skf/mnemo/skide/main/SkideUnitMnemoPanel.java
@@ -17,7 +17,7 @@ import org.toxsoft.uskat.core.connection.*;
 import org.toxsoft.uskat.core.gui.conn.*;
 
 /**
- * Реализация правой панели модуля работы с мнемосхемами.
+ * The SkIDE right panel implementation of the mnemoscheme unit.
  *
  * @author vs
  */
@@ -30,49 +30,16 @@ class SkideUnitMnemoPanel
 
   @Override
   protected Control doCreateControl( Composite aParent ) {
-
     ISkCoreApi coreApi = tsContext().get( ISkConnectionSupplier.class ).defConn().coreApi();
-
     ISkConnection skConn = tsContext().get( ISkConnectionSupplier.class ).defConn();
     IM5Domain m5d = skConn.scope().get( IM5Domain.class );
-    // IM5Model<Object> model = m5d.findModel( ISkMnemoCfg.CLASS_ID );
-    // SkMnemoM5Model model = SkMnemoM5Model.class.cast( m5d.findModel( ISkMnemoCfg.CLASS_ID ) );
-    // SkMnemoM5Model model = SkMnemoM5Model.class.cast( m5().findModel( ISkMnemoCfg.CLASS_ID ) );
-
-    // NICHT! ISkMnemosService mnemoService = tsContext().get( ISkMnemosService.class );
-
     IM5Model<ISkMnemoCfg> model = m5d.getModel( ISkMnemoCfg.CLASS_ID, ISkMnemoCfg.class );
-
     ISkMnemosService mnemoService = coreApi.getService( ISkMnemosService.SERVICE_ID );
     IM5LifecycleManager<ISkMnemoCfg> lm = model.getLifecycleManager( mnemoService );
-
-    IM5CollectionPanel<ISkMnemoCfg> panel;
-    // IM5ItemsProvider<Object> ip = () -> {
-    // IListEdit<Object> result = new ElemArrayList<>();
-    // for( String id : mnemoService.listMnemosIds() ) {
-    // result.add( mnemoService.findMnemo( id ) );
-    // }
-    // return result;
-    // };
     ITsGuiContext ctx = new TsGuiContext( tsContext() );
     OPDEF_IS_FILTER_PANE.setValue( ctx.params(), AV_TRUE );
-    panel = model.panelCreator().createCollEditPanel( ctx, lm.itemsProvider(), lm );
-
+    IM5CollectionPanel<ISkMnemoCfg> panel = model.panelCreator().createCollEditPanel( ctx, lm.itemsProvider(), lm );
     return panel.createControl( aParent );
-
-    // SashForm sash = new SashForm( aParent, SWT.HORIZONTAL );
-    //
-    // CLabel l1 = new CLabel( sash, SWT.BORDER | SWT.CENTER );
-    // l1.setText( "Mnemos sections list" );
-    //
-    // CLabel l2 = new CLabel( sash, SWT.BORDER | SWT.CENTER );
-    // l2.setText( "Current section mnemos list" );
-    //
-    // sash.setWeights( 1, 2 );
-    //
-    //
-    // ISkMnemosService mnemoServ = coreApi.getService( ISkMnemosService.SERVICE_ID );
-    // return sash;
   }
 
 }
